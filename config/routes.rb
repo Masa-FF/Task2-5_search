6927b72a1e51e4406b1ae399ebd92a9cb35b552c
EC2 Default User <ec2-user@ip-172-31-81-73.ec2.internal>
2020-11-05 10:40:10 +0000

    created_v1

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy]
   end
   
+  resources :relationships, only: [:create, :destroy]
+  
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
